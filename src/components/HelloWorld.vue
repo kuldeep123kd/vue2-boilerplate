f79de2abafc959a0afcd563d1f3b4ea5a86d8c5c
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-11-12 00:28:22 +0530

    #store getters

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,6 +29,9 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <h1 style="color: red;">{{ counter }}</h1>
+    <button @click="addOne">Add 1</button>
+    <button @click="addTen">Add 10</button>
   </div>
 </template>
 
@@ -37,7 +40,23 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  computed: {
+    counter() {
+      // return this.$store.getters.getCounter;
+      return this.$store.getters.normalizedCounter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.commit('increase', 1);
+    },
+    addTen() {
+      this.$store.commit('increment', {
+        value: 10
+      });
+    },
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter: 0,
   },
   mutations: {
+    increase (state) {
+      state.counter += 1;
+    },
+    increment (state, payload) {
+      state.counter += payload.value
+    }
   },
   actions: {
+  },
+  getters: {
+    getCounter (state) {
+      return state.counter * 2;
+    },
+    normalizedCounter (state, getters) {
+      // let final_counter = state.counter * 3;
+      let final_counter = getters.getCounter;
+      if(final_counter < 0) {
+        return 0;
+      }
+      if (final_counter > 100) {
+        return 100;
+      }
+      return final_counter;
+    }
   },
   modules: {
   }
